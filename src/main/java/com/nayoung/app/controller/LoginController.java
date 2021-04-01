38be751c6846a5958a873f420c1663e5920d20d0
dpqls519 <dplqs519@naver.com>
2021-04-01 17:37:03 +0900

    add login

---
+++ src/main/java/com/nayoung/app/controller/LoginController.java
@@ -0,0 +1,52 @@
+package com.nayoung.app.controller;
+
+import com.nayoung.app.domain.Login;
+import com.nayoung.app.repository.LoginRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+public class LoginController {
+
+    private final LoginRepository loginRepository;
+
+    public LoginController(LoginRepository loginRepository) {
+        this.loginRepository = loginRepository;
+    }
+
+    // {GET/POST} /students/new
+    @GetMapping("/students/new")
+    public String showStudentForm(Model model) {
+        model.addAttribute("loginForm", new LoginForm());
+        return "students/studentForm";
+    }
+
+    @PostMapping("/students/new")
+    public String createLogin(@Valid LoginForm loginForm, BindingResult result) {
+        if (result.hasErrors()) {
+            return "logins/loginForm";
+        }
+
+        Login login = new Login();
+        login.setName(loginForm.getName());
+        login.setNumber(loginForm.getNumber());
+        login.setAddress(loginForm.getAddress());
+        login.setEmail(loginForm.getEmail());
+        loginRepository.save(login);
+        return "redirect:/logins";
+    }
+
+    // {GET} /students
+    @GetMapping("/logins")
+    public String list(Model model) {
+        List<Login> logins = loginRepository.findAll();
+        model.addAttribute("logins", logins);
+        return "logins/loginList";
+    }
+}
---
+++ src/main/java/com/nayoung/app/controller/LoginForm.java
@@ -0,0 +1,24 @@
+package com.nayoung.app.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class LoginForm {
+    @NotEmpty(message = "이름은 필수입니다.")
+    private String name;
+
+    @NotEmpty(message = "주민번호는 필수입니다.")
+    private String number;
+
+    @NotEmpty(message = "주소는 필수입니다.")
+    private String address;
+
+    @NotEmpty(message = "이메일은 필수입니다.")
+    @Email(message = "이메일 형식을 확인해주세요.")
+    private String email;
+}
